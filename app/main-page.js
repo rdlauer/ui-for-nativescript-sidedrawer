80451e609eabff2b3087a44639af7699d44004c5
Rob Lauer <rdlauer@gmail.com>
2016-12-21 11:10:00 -0600

    initial commit

---
+++ app/main-page.js
@@ -0,0 +1,11 @@
+var view = require("ui/core/view");
+var drawer;
+
+exports.pageLoaded = function(args) {
+	var page = args.object;
+	drawer = view.getViewById(page, "sideDrawer");
+};
+
+exports.toggleDrawer = function() {
+	drawer.toggleDrawerState();
+};
